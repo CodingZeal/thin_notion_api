50f1f89551fbc87972b69c09457779b1a62ce899
Erik Guzman <erik.guzman@codingzeal.com>
2021-07-30 14:12:56 -0700

    Add doc to retrieve page function

--- lib/thin_notion_api/pages.ex
+++ lib/thin_notion_api/pages.ex
@@ -1,6 +1,48 @@
 defmodule ThinNotionApi.Pages do
   import ThinNotionApi.Base
 
+  @doc """
+  Retrieves a Page object using the ID specified.
+
+  Responses contains page properties, not page content. To fetch page content, use the retrieve block children endpoint.
+
+  ## Examples:
+      iex> ThinNotionApi.Pages.retrieve_page("9b4a624d5a18482ab2187e54166edda7")
+      {:ok,
+        %{
+          "archived" => false,
+          "created_time" => "2021-06-11T20:34:00.000Z",
+          "id" => "9b4a624d-5a18-482a-b218-7e54166edda7",
+          "last_edited_time" => "2021-07-30T21:03:00.000Z",
+          "object" => "page",
+          "parent" => %{"type" => "workspace", "workspace" => true},
+          "properties" => %{
+            "title" => %{
+              "id" => "title",
+              "title" => [
+                %{
+                  "annotations" => %{
+                    "bold" => false,
+                    "code" => false,
+                    "color" => "default",
+                    "italic" => false,
+                    "strikethrough" => false,
+                    "underline" => false
+                  },
+                  "href" => nil,
+                  "plain_text" => "Thin Notion Test Workspace",
+                  "text" => %{"content" => "Thin Notion Test Workspace", "link" => nil},
+                  "type" => "text"
+                }
+              ],
+              "type" => "title"
+            }
+          },
+          "url" => "https://www.notion.so/Thin-Notion-Test-Workspace-9b4a624d5a18482ab2187e54166edda7"
+        }
+      }
+  """
+  @spec retrieve_page(String.t()) :: map()
   def retrieve_page(page_id) do
     get("pages/#{page_id}")
   end
