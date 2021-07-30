439f1a82067148002b159cf88d1660758453f9b1
Erik Guzman <erik.guzman@codingzeal.com>
2021-07-30 13:31:34 -0700

    Add stub for when there is no key during test

--- lib/thin_notion_api/utils.ex
+++ lib/thin_notion_api/utils.ex
@@ -6,7 +6,15 @@ defmodule ThinNotionApi.Utils do
   def notion_version,
     do: Application.get_env(:thin_notion_api, :notion_version, "2021-05-13")
 
-  def auth_header, do: [{"Authorization", "Bearer " <> api_key()}]
+  def auth_header do
+    key = if Mix.env() == :test do
+      api_key() || "test_api_key"
+    else
+      api_key()
+    end
+
+    [{"Authorization", "Bearer " <> key}]
+  end
   def version_header, do: [{"Notion-Version", notion_version()}]
   def content_header, do: [{"Content-Type", "application/json"}]
   def request_headers, do: version_header() ++ auth_header()
