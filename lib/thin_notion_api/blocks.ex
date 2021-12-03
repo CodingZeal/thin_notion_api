d83885d04f7b120cb3adfd3d3750d0699a5f6f73
Erik Guzman <erik.guzman@codingzeal.com>
2021-12-03 14:23:27 -0800

    implement block retieve and update

--- lib/thin_notion_api/blocks.ex
+++ lib/thin_notion_api/blocks.ex
@@ -1,12 +1,30 @@
 defmodule ThinNotionApi.Blocks do
   @moduledoc """
   This module contains functions to interact and modify Notion blocks.
+
+  A block object represents content within Notion. Blocks can be text, lists, media, and more. A page is a type of block, too!
   """
 
   import ThinNotionApi.Base
   alias ThinNotionApi.Properties
 
+  @doc """
+  Retrieves a Block object using the ID specified.
+
+  iex> ThinNotionApi.Blocks.retrieve_block(block_id)
+
+  {:ok, %{...}}
+  """
   def retrieve_block(block_id) do
     get("blocks/" <> block_id)
+  end
+
+  @spec retrieve_block_children(String.t(), %{ start_cursor: String.t(), page_size: Integer.t() }) :: any
+  def retrieve_block_children(block_id, params \\ %{}) do
+    get("blocks/" <> block_id <> "/children", params)
+  end
+
+  def update_block(block_id, body_params) do
+    patch("blocks/" <> block_id, body_params)
   end
 end
--- test/thin_notion_api/blocks_test.exs
+++ test/thin_notion_api/blocks_test.exs
@@ -16,4 +16,52 @@ defmodule ThinNotionApi.BlocksTest do
       assert get_in(response, ["child_page", "title"]) == "Thin Notion Test Workspace"
     end
   end
+
+  test "GET retrieve_block_children" do
+    use_cassette "get_retrieve_block_children" do
+      {:ok, response} = Blocks.retrieve_block_children("9b4a624d5a18482ab2187e54166edda7")
+      assert get_in(response, ["object"]) == "list"
+      assert get_in(response, ["results"]) |> List.first() |> get_in(["object"]) == "block"
+      assert get_in(response, ["results"]) |> List.first() |> get_in(["type"]) == "paragraph"
+      assert get_in(response, ["results"])
+      |> List.first()
+      |> get_in(["paragraph", "text"])
+      |> List.first()
+      |> get_in(["plain_text"]) == "Hello world!"
+    end
+  end
+
+
+  test "GET update_block" do
+    use_cassette "get_update_block", match_requests_on: [:request_body] do
+      {:ok, response} = Blocks.update_block("c4c027f4ea7c41c5908d63a7f5a9c32c", %{
+        paragraph: %{
+          text: [%{
+            type: "text",
+            text: %{
+              content: "Hello DOGE!",
+            }
+          }],
+        }
+      })
+      assert get_in(response, ["paragraph", "text"])
+      |> List.first()
+      |> get_in(["plain_text"]) == "Hello DOGE!"
+
+      {:ok, response} = Blocks.update_block("c4c027f4ea7c41c5908d63a7f5a9c32c", %{
+        paragraph: %{
+          text: [%{
+            type: "text",
+            text: %{
+              content: "Hello world!",
+            }
+          }],
+        }
+      })
+
+      assert get_in(response, ["paragraph", "text"])
+      |> List.first()
+      |> get_in(["plain_text"]) == "Hello world!"
+    end
+  end
 end
