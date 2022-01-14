3a36e6c4214f9ea340afb914f3d4f865add13f0d
Erik Guzman <erik.guzman@codingzeal.com>
2022-01-14 14:15:06 -0800

    Update user module docs and add back removed code

--- lib/thin_notion_api/users.ex
+++ lib/thin_notion_api/users.ex
@@ -4,15 +4,16 @@ defmodule ThinNotionApi.Users do
   """
 
   import ThinNotionApi.Base
+  alias ThinNotionApi.Types
 
   @doc """
   Retrieves a User using the ID specified.
 
-  iex> ThinNotionApi.Users.retrieve_user("12345")
-
-  {:ok, %{...}}
+  ## Examples
+      iex> ThinNotionApi.Users.retrieve_user("12345")
+      {:ok, %{...}}
   """
-  @spec retrieve_user(String.t()) :: any
+  @spec retrieve_user(String.t()) :: Types.Response.response()
   def retrieve_user(user_id) do
     get("users/" <> user_id)
   end
@@ -22,17 +23,26 @@ defmodule ThinNotionApi.Users do
 
   See Pagination for details about how to use a cursor to iterate through the list.
 
-  iex> ThinNotionApi.Users.list_all_users(params)
+  ## Examples
+      iex> ThinNotionApi.Users.list_all_users(params)
 
-  {:ok, %{...}}
+      {:ok, %{...}}
   """
-  @spec list_all_users(map) :: any
+  @spec list_all_users(Types.PaginationParams.t() | %{}) :: Types.Response.response()
   def list_all_users(params \\ %{}) do
     get("users", params)
   end
 
-  # @spec retrieve_user_bot :: any
-  # def retrieve_user_bot do
-  #   get("users/me")
-  # end
+  @spec retrieve_user_bot :: Types.Response.response()
+  @doc """
+  Retrieves the bot User associated with the API token provided in the authorization header. The bot will have an owner field with information about the person who authorized the integration.
+
+  ## Examples
+      iex> ThinNotionApi.Users.retrieve_user_bot()
+
+      {:ok, %{...}}
+  """
+  def retrieve_user_bot do
+    get("users/me")
+  end
 end
