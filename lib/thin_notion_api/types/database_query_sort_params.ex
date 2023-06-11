f8b34742501c429e42b0b3106a28d5e707c2dc7a
Erik Guzman <erik.guzman@codingzeal.com>
2023-06-11 10:33:17 -0700

    Fix typos found in sort params for descending

--- lib/thin_notion_api/types/database_query_sort_params.ex
+++ lib/thin_notion_api/types/database_query_sort_params.ex
@@ -1,10 +1,14 @@
 defmodule ThinNotionApi.Types.DatabaseQuerySortParams do
+  @moduledoc """
+  Defines the options for the sorting the database query results
+  """
+
   @typedoc """
   Parameter options for the sorting the database query results
   """
   @type t :: %__MODULE__{
     property: String.t(),
-    direction: :ascending | :decending,
+    direction: :ascending | :descending,
     timestamp: :created_time | :last_edited_time
   }
 
--- lib/thin_notion_api/types/search_sort_params.ex
+++ lib/thin_notion_api/types/search_sort_params.ex
@@ -1,10 +1,13 @@
 defmodule ThinNotionApi.Types.SearchSortParams do
+  @moduledoc """
+  Defines the options for the sorting the search results
+  """
+
   @typedoc """
   Parameter options for the sorting the search results
   """
-
   @type t :: %__MODULE__{
-    direction: :ascending | :decending,
+    direction: :ascending | :descending,
     timestamp: String.t(),
   }
 
